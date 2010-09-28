a8a43ca4d40b161d806d2db5ce155c399f968d88
Steve Tarzia <steve@stevetarzia.com>
2010-09-28 10:45:53 -0500

    added simpleUI project which tests the Fingerprinter class.

---
+++ simpleUI/Classes/simpleUIAppDelegate.mm
@@ -0,0 +1,127 @@
+//
+//  simpleUIAppDelegate.m
+//  simpleUI
+//
+//  Created by Stephen Tarzia on 9/28/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "simpleUIAppDelegate.h"
+#import "Fingerprinter.h"
+#import <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+void printFingerprint( Fingerprint* fingerprint ){
+	for( unsigned int i=0; i<Fingerprinter::fpLength; ++i ){
+		cout << (*fingerprint)[i] << ' ';
+	}
+	cout << endl;
+}
+
+void test(){
+	Fingerprinter fp;
+	
+	// record a new fingerprint using the microphone
+	Fingerprint* observed = fp.recordFingerprint();
+	cout << "Newly observed fingerprint:" <<endl;
+	printFingerprint(observed);
+	
+	sleep(1);
+	
+	// query for a list of matches
+	cout << endl << "DB Matches:" <<endl;
+	int NUM_MATCHES = 3;
+	QueryResult* qr = fp.queryMatches( observed, NUM_MATCHES );
+	for( int i=0; i<NUM_MATCHES; i++ ){
+		cout << "match #" << i << '\t'
+		<< "uid=" << (*qr)[i].uid << '\t' 
+		<< "name=" << fp.queryName( (*qr)[i].uid ) << '\t' 
+		<< "confidence=" << (*qr)[i].confidence << '\t'
+		<< "fingerprint= ";
+		printFingerprint( fp.queryFingerprint( (*qr)[i].uid ) );
+	}
+	
+	// assuming that we were not satisfied with any of the results, add this as a new room
+	fp.insertFingerprint( observed, string( "newRoom" ) );
+	
+	delete observed;
+	delete qr;
+}
+
+@implementation simpleUIAppDelegate
+
+@synthesize window;
+
+
+#pragma mark -
+#pragma mark Application lifecycle
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+    
+    // Override point for customization after application launch.
+	test();
+	
+    
+    [window makeKeyAndVisible];
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+
+#pragma mark -
+#pragma mark Memory management
+
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
+    /*
+     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
+     */
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
