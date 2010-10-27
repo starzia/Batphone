1af9af198cec83bad88353a7003fd1d08c356fec
Steve Tarzia <steve@stevetarzia.com>
2010-10-26 23:31:41 -0500

    Fixed "red bar" warning due to recording still running during sleep.

--- simpleUI/Classes/simpleUIAppDelegate.mm
+++ simpleUI/Classes/simpleUIAppDelegate.mm
@@ -294,6 +294,7 @@ static const int numCandidates = 3;
      Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
      Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
      */
+	self.fp->stopRecording();
 }
 
 
@@ -318,6 +319,7 @@ static const int numCandidates = 3;
     /*
      Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
      */
+	self.fp->startRecording();
 }
 
 
